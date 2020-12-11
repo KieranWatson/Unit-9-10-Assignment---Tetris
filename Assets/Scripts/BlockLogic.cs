bce79d9f7df65c4ff658ad370c0bd3688eb53991
Kieran Watson <61968335+KieranWatson@users.noreply.github.com>
2020-12-11 10:20:45 +0000

    Initial commit

---
+++ Assets/Scripts/BlockLogic.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockLogic : MonoBehaviour
+{
+    bool movable = true;
+    float timer = 0f;
+
+    public GameObject rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+
+    bool CheckValid()
+    {
+        foreach(Transform subBlock in rig.transform)
+        {
+            if(subBlock.transform.position.x > GameLogic.width || subBlock.transform.position.x < 0 || subBlock.transform.position.y < 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movable)
+        {
+            //Update the timer.
+            timer += 1 * Time.deltaTime;
+
+            //Drop and Soft Drop
+            if(Input.GetKey(KeyCode.DownArrow) && timer > GameLogic.quickDropTime)
+            {
+                gameObject.transform.position -= new Vector3(0, 1, 0);
+                timer = 0;
+                if (!CheckValid())
+                {
+                    gameObject.transform.position += new Vector3(0, 1, 0);
+                }
+            }
+            else if(timer > GameLogic.dropTime)
+            {
+                gameObject.transform.position -= new Vector3(0, 1, 0);
+                timer = 0;
+                if (!CheckValid())
+                {
+                    gameObject.transform.position += new Vector3(0, 1, 0);
+                }
+            }
+
+            //Moving Side to Side.
+            if(Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                gameObject.transform.position += new Vector3(1, 0, 0);
+                if (!CheckValid())
+                {
+                    gameObject.transform.position -= new Vector3(1, 0, 0);
+                }
+            }
+            else if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                gameObject.transform.position -= new Vector3(1, 0, 0);
+                if (!CheckValid())
+                {
+                    gameObject.transform.position += new Vector3(1, 0, 0);
+                }
+            }
+
+            //Rotation
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                rig.transform.eulerAngles -= new Vector3(0, 0, 90);
+                if (!CheckValid())
+                {
+                    rig.transform.eulerAngles += new Vector3(0, 0, 90);
+                }
+            }
+            else if(Input.GetKeyDown(KeyCode.D))
+            {
+                rig.transform.eulerAngles += new Vector3(0, 0, 90);
+                if (!CheckValid())
+                {
+                    rig.transform.eulerAngles -= new Vector3(0, 0, 90);
+                }
+            }
+        } 
+    }
+}
---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    //This will make the piece slowly fall to the floor when it is used.
+    public static float dropTime = 0.9f;
+
+    //This will make the piece fall faster when activated.
+    public static float quickDropTime = 0.05f;
+
+    //Board
+    public static int width = 15, height = 30;
+
+    //Stores the different pieces.
+    public GameObject[] blocks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnBlock()
+    {
+        float guess = Random.Range(0, 1f);
+        guess *= blocks.Length;
+        Instantiate(blocks[Mathf.FloorToInt(guess)]);
+    }
+}
