27fd143f8a3fb9728b364c23242883e03d5922bb
GodsNinja12 <61968335+GodsNinja12@users.noreply.github.com>
2020-12-13 11:51:39 +0000

    Pause Menu + Sorted the buttons out.
    Added a pause menu as well as adjusted the button placement.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    void Update()
+    {
+        Restart();
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
